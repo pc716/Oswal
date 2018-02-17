42dc0329faccd4e13a781de74755ffba71afc66a
Chea Hoon Heo <cheo2@gmu.edu>
2018-02-17 18:46:03 -0500

    Started DatabaseClient class

--- src/main/java/com/oswal/backend/BackendApplication.java
+++ src/main/java/com/oswal/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.oswal.backend;
 
+import com.oswal.backend.controller.DatabaseClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +9,6 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+        new DatabaseClient();
 	}
 }
--- src/main/java/com/oswal/backend/controller/DatabaseClient.java
+++ src/main/java/com/oswal/backend/controller/DatabaseClient.java
@@ -1,15 +1,33 @@
 package com.oswal.backend.controller;
 
+import com.mongodb.DB;
+import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
+import org.bson.Document;
 
 
 public class DatabaseClient {
 
     private MongoClient mongo_client;
+    private MongoDatabase database;
 
     public DatabaseClient(){
         mongo_client = new MongoClient("localhost", 27017);
+        database = mongo_client.getDatabase("oswalDB");
+        database.createCollection("users");
+        Document doc = new Document("email" ,"djung8@gmu.edu").append("password", "123456");
+        database.getCollection("users").insertOne(doc);
     }
+
+    public boolean checkUser(String email,String password){
+        database.getCollection("users").find( { email )
+        return false;
+    }
+
+
+
+
+
 }
