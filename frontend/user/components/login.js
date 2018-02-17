fe50e4fb011e9c95028eb791f857442fd9d67c70
Dong Woo Jung <djung8@gmu.edu>
2018-02-17 03:53:48 -0500

    Login page elements added

--- frontend/user/components/login.js
+++ frontend/user/components/login.js
@@ -1,24 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Input, Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {
+    StyleSheet,
+    ScrollView,
+    Text,
+    TextInput,
+    View
+} from 'react-native';
 
 class Login extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>LOGIN</Text>
-        
-      </View>
-    );
+      <View>
+      <Input
+        placeholder='Username'
+        displayError={false}
+        errorStyle={{ color: 'red' }}
+        errorMessage='ENTER A VALID ERROR HERE'
+        containerStyle={ styles.oval }
+      />
+      <Input
+        placeholder='Password'
+        displayError={false}
+        errorStyle={{ color: 'red' }}
+        errorMessage='ENTER A VALID ERROR HERE'
+        containerStyle={ styles.oval }
+      />
+    <Button
+      text='Login'
+      buttonStyle={ styles.buttonOval }
+    />
+    </View>
+        );
   }
 }
 
+
 const styles = StyleSheet.create({
+  buttonOval: {
+    borderRadius: 25,
+    borderWidth: 1,
+    borderColor: '#d6d7da',
+    width: 300,
+    height: 50,
+    margin: 7
+  },
+  oval: {
+    borderRadius: 25,
+    borderWidth: 1,
+    borderColor: '#d6d7da',
+    width: 300,
+    height: 50,
+    margin: 7
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
 
 export default Login;
