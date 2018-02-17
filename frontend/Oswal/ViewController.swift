7aea9c30c6ce58cecedbedd67371cbe758dd9fc9
Tommy Lee <tlee32@gmu.edu>
2018-02-17 16:44:54 -0500

    login functionality

--- frontend/Oswal/ViewController.swift
+++ frontend/Oswal/ViewController.swift
@@ -7,6 +7,12 @@
 //
 
 import UIKit
+import JSONEncoder
+
+struct Post: Codable {
+    let email: String
+    let password: String
+}
 
 class ViewController: UIViewController {
 
@@ -56,12 +62,75 @@ class ViewController: UIViewController {
             }
         // Successful login
         } else {
-            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
-            let nextViewController = storyBoard.instantiateViewController(withIdentifier: "ProfileViewController") as! ProfileViewController
+            if(loginSuccessful()) {
+                let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+                let nextViewController = storyBoard.instantiateViewController(withIdentifier: "ProfileViewController") as! ProfileViewController
+                
+                // Go to next view
+                self.present(nextViewController, animated:true, completion:nil)
+            } else {
+                displayAlert(msg:"you suck")
+            }
             
-            // Go to next view
-            self.present(nextViewController, animated:true, completion:nil)
+            
         }
+    }
+    
+    func loginSuccessful() {
+        let user = Post(email: "cheo2@gmu.edu", password: "123456");
+        submitPost(post: user) { (error) in
+            if let error = error {
+                fatalError(error.localizedDescription);
+                return false;
+            }
+        return true;
+    }
+    
+    // We'll need a completion block that returns an error if we run into any problems
+    func submitPost(post: Post, completion:((Error?) -> Void)?) {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "http"
+        urlComponents.host = "10.194.114.209:8080"
+        urlComponents.path = "/login"
+        guard let url = urlComponents.url else { fatalError("Could not create URL from components") }
+        
+        // Specify this request as being a POST method
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        // Make sure that we include headers specifying that our request's HTTP body
+        // will be JSON encoded
+        var headers = request.allHTTPHeaderFields ?? [:]
+        headers["Content-Type"] = "application/json"
+        request.allHTTPHeaderFields = headers
+        
+        // Now let's encode out Post struct into JSON data...
+        let encoder = JSONEncoder()
+        do {
+            let jsonData = try encoder.encode(post)
+            // ... and set our request's HTTP body
+            request.httpBody = jsonData
+            print("jsonData: ", String(data: request.httpBody!, encoding: .utf8) ?? "no body data")
+        } catch {
+            completion?(error)
+        }
+        
+        // Create and run a URLSession data task with our JSON encoded POST request
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        let task = session.dataTask(with: request) { (responseData, response, responseError) in
+            guard responseError == nil else {
+                completion?(responseError!)
+                return
+            }
+            
+            // APIs usually respond with the data you just sent in your POST request
+            if let data = responseData, let utf8Representation = String(data: data, encoding: .utf8) {
+                print("response: ", utf8Representation)
+            } else {
+                print("no readable data received in response")
+            }
+        }
+        task.resume()
     }
     
     func displayAlert(msg:String) {
@@ -76,3 +145,4 @@ class ViewController: UIViewController {
 
 }
 
+}
