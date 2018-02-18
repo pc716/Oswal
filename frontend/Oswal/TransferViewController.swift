cb411eaf7a8904cf16b4da2574e972aada05a330
Tommy Lee <tlee32@gmu.edu>
2018-02-17 21:16:44 -0500

    lol

---
+++ frontend/Oswal/TransferViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TransferViewController.swift
+//  Oswal
+//
+//  Created by Tommy Lee on 2/17/18.
+//  Copyright © 2018 Tommy Lee. All rights reserved.
+//
+
+import UIKit
+
+class TransferViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
