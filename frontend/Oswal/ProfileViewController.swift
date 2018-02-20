25efd71aa6ecbeb2d08e9501b12e38cf2fef2b62
Chea Hoon Heo <cheo2@gmu.edu>
2018-02-19 19:45:38 -0500

    Added side menu to frontend

--- frontend/Oswal/ProfileViewController.swift
+++ frontend/Oswal/ProfileViewController.swift
@@ -12,6 +12,38 @@ class ProfileViewController: UIViewController {
 
     @IBOutlet weak var customView: UIView!
     
+    @IBOutlet var leadingC: NSLayoutConstraint!
+    @IBOutlet var trailingC: NSLayoutConstraint!
+    
+    @IBOutlet var mainView: UIView!
+    
+    var hamburgerMenuIsVisible = false
+    
+    @IBAction func hamburgerBtnTapped(_ sender: Any) {
+        //if the hamburger menu is NOT visible, then move the ubeView back to where it used to be
+        if !hamburgerMenuIsVisible {
+            leadingC.constant = 250
+            //this constant is NEGATIVE because we are moving it 150 points OUTWARD and that means -150
+            trailingC.constant = -250
+            
+            //1
+            hamburgerMenuIsVisible = true
+        } else {
+            //if the hamburger menu IS visible, then move the ubeView back to its original position
+            leadingC.constant = 0
+            trailingC.constant = 0
+            
+            //2
+            hamburgerMenuIsVisible = false
+        }
+        
+        UIView.animate(withDuration: 0.2, delay: 0.0, options: .curveEaseIn, animations: {
+            self.view.layoutIfNeeded()
+        }) { (animationComplete) in
+            print("The animation is complete!")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
